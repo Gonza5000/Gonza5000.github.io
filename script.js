b906368253612d1a16b2b53a5bfafb8bce067583
Gonza5000 <5000.gonza.5000@gmail.com>
2024-11-06 10:15:33 +0100

    bola

---
+++ script.js
@@ -0,0 +1,8 @@
+const canvas = document.querySelector("canvas")
+const ctx = canvas.getContext("2d")
+
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+let Bola = new Bola(canvas.width/2, canvas.height/2)
+Bola.dibujar()
