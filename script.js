1bc319fd189cf9c52e34cc0f620533265b003aa3
Gonza5000 <5000.gonza.5000@gmail.com>
2024-11-11 11:33:12 +0100

    animar

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ const ctx = canvas.getContext("2d")
 canvas.width = innerWidth
 canvas.height = innerHeight
 
-let Bola = new Bola(canvas.width/2, canvas.height/2)
-Bola.dibujar()
-Bola.mover()
+let bola = new Bola(canvas.width/2, canvas.height/2)
+
+function animar() {
+    bola.dibujar()
+    bola.mover()
+
+    requestAnimationFrame(animar)
+}
+
+animar()
