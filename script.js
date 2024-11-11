17a5e81fa3da98afe75718bcb8996ed8c89f384d
Gonza5000 <5000.gonza.5000@gmail.com>
2024-11-11 11:55:35 +0100

    finsh

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ canvas.height = innerHeight
 
 let bolas = []
 
-for(let i = 0; i < 50; i++){
+for(let i = 0; i < 20; i++){
     bolas.push(new Bola(canvas.width/2, canvas.height/2))
 }
 
@@ -15,6 +15,20 @@ function animar() {
     ctx.clearRect(0,0, canvas.width, canvas.height)
 
     bolas.forEach(bola => {
+        bolas.forEach(bola2 => {
+            let dx = bola2.x - bola.x
+            let dy = bola2.y - bola.y
+            let dist = Math.sqrt(dx ** 2 + dy ** 2)
+
+            if(dist < 350) {
+                ctx.beginPath()
+                ctx.moveTo(bola.x,bola.y)
+                ctx.lineTo(bola2.x, bola2.y)
+                ctx.stroke()
+                ctx.closePath()
+            }
+        })
+
         bola.dibujar()
         bola.mover()
     })
