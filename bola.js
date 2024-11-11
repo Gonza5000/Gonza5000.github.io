2cbeea6de1fec48128899151b8d5c652a3c139ec
Gonza5000 <5000.gonza.5000@gmail.com>
2024-11-11 11:45:45 +0100

    colisiones

--- bola.js
+++ bola.js
@@ -19,5 +19,13 @@ class Bola{
     mover(){
         this.x += this.dirX * this.velocidad
         this.y += this.dirY * this.velocidad
+
+        if(this.x + this.radio > canvas.width || this.x < 0){
+            this.dirX *= -1
+        }
+
+        if(this.y + this.radio > canvas.height || this.y < 0){
+            this.dirY *= -1
+        }
     }
 }
--- script.js
+++ script.js
@@ -6,11 +6,13 @@ canvas.height = innerHeight
 
 let bolas = []
 
-for(let i = 0; i < 5; i++){
+for(let i = 0; i < 50; i++){
     bolas.push(new Bola(canvas.width/2, canvas.height/2))
 }
 
 function animar() {
+
+    ctx.clearRect(0,0, canvas.width, canvas.height)
 
     bolas.forEach(bola => {
         bola.dibujar()
